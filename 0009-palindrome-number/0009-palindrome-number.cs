8628f73e91804280b07e242cf28736e6bf1eb67d
ART4S <ChuletsVA@gmail.com>
2023-09-21 16:39:10 +0200

    Time: 42 ms (41.06%), Space: 29.7 MB (72.15%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public bool IsPalindrome(int x)
+    {
+        string s = x.ToString();
+
+        int i = 0;
+        int j = s.Length - 1;
+
+        while (i < j)
+        {
+            if (s[i] != s[j]) return false;
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+}
