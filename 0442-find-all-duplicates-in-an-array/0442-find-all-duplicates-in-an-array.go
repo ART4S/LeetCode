1eb6b966509c1caf7adf1c5bf79871af327e3007
Vyacheslav Chulets <ChuletsVA@gmail.com>
2023-11-09 19:00:25 +0200

    Time: 45 ms (73.28%), Space: 8.2 MB (25.19%) - LeetHub

---
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.go
@@ -0,0 +1,19 @@
+func findDuplicates(nums []int) []int {
+	for i := range nums {
+		for nums[i]-1 != i && nums[i] != nums[nums[i]-1] {
+			temp := nums[nums[i]-1]
+			nums[nums[i]-1] = nums[i]
+			nums[i] = temp
+		}
+	}
+
+	res := make([]int, 0)
+
+	for i := range nums {
+		if nums[i]-1 != i {
+			res = append(res, nums[i])
+		}
+	}
+
+	return res
+}
