be6f4adc3784a771589bfa9673476f4e889726bc
Vyacheslav Chulets <ChuletsVA@gmail.com>
2023-11-27 23:52:02 +0200

    Time: 177 ms (35.46%), Space: 65.9 MB (5.58%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cs
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cs
@@ -1,6 +1,48 @@
 public class Solution {
     public IList<int> FindClosestElements(int[] arr, int k, int x)
     {
+        return FindClosestElements_BinarySearch(arr, k, x);
+    }
+
+    private IList<int> FindClosestElements_BinarySearch(int[] arr, int k, int x)
+    {
+        int l = 0;
+        int r = arr.Length - 1;
+
+        while (l < r)
+        {
+            int m = l + (r - l) / 2;
+
+            if (arr[m] < x)
+                l = m + 1;
+            else
+                r = m;
+        }
+
+        l--;
+
+        var deque = new LinkedList<int>();
+
+        while (k-- > 0)
+        {
+            if (l >= 0 && r < arr.Length)
+            {
+                if (Math.Abs(arr[l] - x) <= Math.Abs(arr[r] - x))
+                    deque.AddFirst(arr[l--]);
+                else
+                    deque.AddLast(arr[r++]);
+            }
+            else if (l >= 0)
+                deque.AddFirst(arr[l--]);
+            else
+                deque.AddLast(arr[r++]);
+        }
+
+        return deque.ToArray();
+    }
+
+    private IList<int> FindClosestElements_TwoPointers(int[] arr, int k, int x)
+    {
         int j = k - 1;
 
         for (int i = k; i < arr.Length; i++)
