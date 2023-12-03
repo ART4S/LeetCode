767ed3bf3b4b4f0e6011f0789003df9b206a6ec9
Vyacheslav Chulets <ChuletsVA@gmail.com>
2023-12-03 21:02:44 +0200

    Time: 102 ms (95.80%), Space: 46.9 MB (85.43%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cs
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cs
@@ -1,5 +1,11 @@
-public class Solution {
+public class Solution
+{
     public int MinSubArrayLen(int target, int[] nums)
+    {
+        return MinSubArrayLen_TwoPointers(target, nums);
+    }
+
+    private int MinSubArrayLen_TwoPointers(int target, int[] nums)
     {
         int minLen = int.MaxValue;
 
@@ -12,6 +18,44 @@ public class Solution {
                 minLen = Math.Min(minLen, r - l + 1);
 
                 sum -= nums[l++];
+            }
+        }
+
+        return minLen == int.MaxValue ? 0 : minLen;
+    }
+
+    private int MinSubArrayLen_BinSearch(int target, int[] nums)
+    {
+        int minLen = int.MaxValue;
+
+        var sums = new int[nums.Length + 1];
+
+        int sum = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            sum += nums[i];
+
+            sums[i] = sum;
+
+            if (sum >= target)
+            {
+                int l = 0;
+                int r = i;
+
+                while (l < r)
+                {
+                    int m = l + (r - l) / 2 + 1;
+
+                    int s = sum - (m == 0 ? 0 : sums[m - 1]); // sum from m to i
+
+                    if (s >= target)
+                        l = m;
+                    else
+                        r = m - 1;
+                }
+
+                minLen = Math.Min(minLen, i - r + 1);
             }
         }
 
