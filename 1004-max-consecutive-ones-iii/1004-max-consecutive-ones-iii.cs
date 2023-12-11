ceceac1cce73e11d6493118ea3d78c1ba5f34126
Vyacheslav Chulets <ChuletsVA@gmail.com>
2023-12-11 13:20:51 +0200

    Time: 148 ms (39.41%), Space: 60.6 MB (49.48%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cs
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cs
@@ -1,6 +1,11 @@
 public class Solution {
     public int LongestOnes(int[] nums, int k)
     {
+        return LongestOnes_SlidingWindow(nums, k);
+    }
+
+    private int LongestOnes_SlidingWindow(int[] nums, int k)
+    {
         int max = 0;
         int z = 0;
 
@@ -20,6 +25,41 @@ public class Solution {
             }
 
             max = Math.Max(max, r - l + 1);
+        }
+
+        return max;
+    }
+
+    private int LongestOnes_PrefixSum(int[] nums, int k)
+    {
+        var ps = new int[nums.Length];
+
+        int sum = 0;
+        int max = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            sum += 1 - nums[i];
+
+            ps[i] += sum;
+
+            int l = 0;
+            int r = i;
+
+            while (l < r)
+            {
+                int m = l + (r - l) / 2;
+
+                if (ps[i] - (m > 0 ? ps[m - 1] : 0) > k)
+                    l = m + 1;
+                else
+                    r = m;
+            }
+
+            if (ps[i] - (l > 0 ? ps[l - 1] : 0) <= k)
+            {
+                max = Math.Max(max, i - l + 1);
+            }
         }
 
         return max;
