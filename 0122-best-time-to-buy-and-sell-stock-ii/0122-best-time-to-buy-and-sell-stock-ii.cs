036e403e0af0ff49a04f6e8060274b71342b6c4e
Vyacheslav Chulets <ChuletsVA@gmail.com>
2023-12-13 12:10:38 +0200

    Time: 85 ms (34.86%), Space: 39.6 MB (70.13%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cs
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cs
@@ -1,5 +1,10 @@
 public class Solution {
-    public int MaxProfit(int[] prices) {
+    public int MaxProfit(int[] prices)
+    {
+        return MaxProfit_Greedy(prices);
+    }
+
+    private int MaxProfit_Greedy(int[] prices) {
         int profit = 0;
 
         for(int i = 1; i < prices.Length; i++)
@@ -8,5 +13,36 @@ public class Solution {
         }
 
         return profit;
+    }
+
+    private int MaxProfit_DP(int[] prices)
+    {
+        var memo = new int[prices.Length][];
+
+        for (int i = 0; i < memo.Length; i++)
+        {
+            memo[i] = new [] { -1, -1 };
+        }
+
+        return solve(0, 0);
+
+        int solve(int i, int buy)
+        {
+            if (i == prices.Length) return 0;
+
+            if (memo[i][buy] == -1)
+            {
+                if (buy == 0)
+                {
+                    memo[i][buy] = Math.Max(solve(i + 1, 1) - prices[i], solve(i + 1, 0));
+                }
+                else
+                {
+                    memo[i][buy] = Math.Max(prices[i] + solve(i + 1, 0), solve(i + 1, 1));
+                }
+            }
+
+            return memo[i][buy];
+        }
     }
 }
