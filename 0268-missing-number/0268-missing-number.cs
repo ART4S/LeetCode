dcaba19838fbdd1cd8d2a7e0e5b182798891fa41
Vyacheslav Chulets <ChuletsVA@gmail.com>
2023-12-25 21:30:52 +0200

    Time: 94 ms (65.28%), Space: 45.4 MB (19.12%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cs
@@ -0,0 +1,60 @@
+public class Solution {
+    public int MissingNumber(int[] nums) 
+    {
+        return MissingNumber_Xor(nums);
+    }
+
+    private int MissingNumber_Xor(int[] nums) 
+    {
+        int xor = 0;
+
+        for(int i = 1; i <= nums.Length; i++)
+        {
+            xor ^= i;
+        }
+
+        foreach(var num in nums)
+        {
+            xor ^= num;
+        }
+
+        return xor;
+    }
+
+    private int MissingNumber_HashSet(int[] nums)
+    {
+        var hs = new HashSet<int>();
+
+        for(int i = 0; i <= nums.Length; i++)
+        {
+            hs.Add(i);
+        }
+
+        foreach(var num in nums)
+        {
+            hs.Remove(num);
+        }
+
+        return hs.First();
+    }
+
+    private int MissingNumber_Arr(int[] nums) 
+    {
+        var arr = new bool[nums.Length + 1];
+
+        foreach(var num in nums)
+        {
+            arr[num] = true;
+        }
+
+        for(int i = 0; i < arr.Length; i++)
+        {
+            if (!arr[i])
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
