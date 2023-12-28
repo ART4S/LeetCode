75e5262b84fda413049c29c978bfa9a2cb572883
Vyacheslav Chulets <ChuletsVA@gmail.com>
2023-12-28 16:34:48 +0200

    Time: 590 ms (100.00%), Space: 63.8 MB (100.00%) - LeetHub

---
+++ 0870-advantage-shuffle/0870-advantage-shuffle.cs
@@ -0,0 +1,31 @@
+public class Solution {
+    // n logn
+    public int[] AdvantageCount(int[] nums1, int[] nums2)
+    {
+        var n = nums1.Length;
+
+        var n1sorted = nums1.ToArray();
+        var n2sorted = nums2.Select((x, i) => (Num: x, Ind: i)).ToArray();
+
+        Array.Sort(n1sorted);
+        Array.Sort(n2sorted, (a, b) => a.Num - b.Num);
+
+        for (int i = 0, l = 0, r = n - 1; i < n; i++)
+        {
+            if (n1sorted[i] > n2sorted[l].Num)
+            {
+                nums1[n2sorted[l].Ind] = n1sorted[i];
+
+                l++;
+            }
+            else
+            {
+                nums1[n2sorted[r].Ind] = n1sorted[i];
+
+                r--;
+            }
+        }
+
+        return nums1;
+    }
+}
