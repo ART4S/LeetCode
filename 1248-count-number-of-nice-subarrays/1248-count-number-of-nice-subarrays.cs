ebd46eaf336c890bc50c5421d1876f3ad2206e52
Vyacheslav Chulets <ChuletsVA@gmail.com>
2024-01-18 15:51:27 +0200

    Time: 312 ms (63.41%), Space: 54.5 MB (21.95%) - LeetHub

---
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cs
@@ -0,0 +1,64 @@
+public class Solution {
+    public int NumberOfSubarrays(int[] nums, int k)
+    {
+        return NumberOfSubarrays_TwoPointers(nums, k);
+    }
+
+    // n
+    private int NumberOfSubarrays_TwoPointers(int[] nums, int k)
+    {
+        int n = nums.Length;
+        int odd = 0;
+        int evenBefore = 0;
+        int res = 0;
+
+        for (int i = 0, j = 0; i < n; i++)
+        {
+            if (nums[i] % 2 != 0) odd++;
+
+            if (odd > k)
+            {
+                j++;
+                odd--;
+                evenBefore = 0;
+            }
+
+            if (odd == k)
+            {
+                while (j < i && nums[j] % 2 == 0)
+                {
+                    j++;
+                    evenBefore++;
+                }
+
+                res += evenBefore + 1;
+            }
+        }
+
+        return res;
+    }
+
+    // n
+    private int NumberOfSubarrays_PrefixSum(int[] nums, int k)
+    {
+        var pref_sum = new Dictionary<int, int>() { [0] = 1 };
+
+        int odd = 0;
+        int res = 0;
+
+        foreach (var num in nums)
+        {
+            if (num % 2 != 0) odd++;
+
+            pref_sum.TryAdd(odd, 0);
+            pref_sum[odd]++;
+
+            if (pref_sum.TryGetValue(odd - k, out var cnt))
+            {
+                res += cnt;
+            }
+        }
+
+        return res;
+    }
+}
